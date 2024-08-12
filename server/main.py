9d9388c4008453b05a4a2735a8a2ad961554f1a4
Ashish Lal <ashishlal2003@gmail.com>
2024-08-13 01:17:52 +0530

    llm added

--- server/main.py
+++ server/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from dotenv import load_dotenv
+from routes.llm import llm_bp
 
 load_dotenv()
 
 app = Flask(__name__)
+
+app.register_blueprint(llm_bp, url_prefix='/llm')
 
 @app.route('/')
 def hello_world():
